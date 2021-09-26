4f31494f60986e5d32d13300ac663d05f947c777
rakky21 <rakky26@gmail.com>
2021-09-26 17:17:04 -0400

    Finalized the styling of the calender as well as added the color coordinating based on the time of the day. Will work on local storage in order for the message box to persist

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,75 +1,38 @@
-// moment().format('MMMM Do YYYY, h:mm:ss a');
-
+console.log("hello")
 
 // fetch('https://momentjs.com/downloads/moment-with-locales.js')
 //     .then(response => response.json())
 //     .then(data => console.log(data));
-
-
 // const d = new Date();
 // document.getElementById("demo").innerHTML = d.getDate();
-
-
 // var currentDay = document.getElementById("currentDay").value;
 // document
+// fetch(
+//     'https://momentjs.com/downloads/moment-with-locales.js'
+// )
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (response) {
+//         // console.log(response)
 
-fetch(
-    'https://momentjs.com/downloads/moment-with-locales.js'
-)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        // console.log(response)
-        var responseContainerEl = document.querySelector('#currentDay');
 
-        var gifImg = document.createElement('h2');
 
-        // gifImg.setAttribute('src', response.data.image_url);
 
-        responseContainerEl.appendChild(jummbotron);
-    });
-
+//         var gifImg = document.createElement('h2');
+//         // gifImg.setAttribute('src', response.data.image_url);
+//         responseContainerEl.appendChild(jummbotron);
+//     });
 // var today = new Date();
-
 // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
+// grabbing 21 'currentday' for it to be displayed/
+        var responseContainerEl = document.querySelector('#currentDay');
+// what we want to display
+var  currentDay = moment().format('MMMM Do YYYY');
+responseContainerEl.textContent = currentDay
+var currentHour = moment().hours()
 
-
-// need time api so the boxes turn grey once the time has passed
-
-// 🔒 locked
-
-// 🔓 unlocked
-
-// ✅ scheduled 
-
-
-
-
-
-
-
-
-
-// WIDTH OF THE COLUM
-// $grid-breakpoints: (
-//     xs: 0,
-//     sm: 576px,
-//     md: 768px,
-//     lg: 992px,
-//     xl: 1200px,
-//     xxl: 1400px
-//   );
-// @include media - breakpoint - up(lg) { .custom - class {
-//     display: none;
-// }
-//     @include media - breakpoint - up(sm) {
-//     .custom - class {
-//             display: block;
-//         }
-//     }
-// }
 
 
 
@@ -78,9 +41,35 @@ fetch(
 // var timeSlots = slots
 // for (var i = 0 ; i < 24 ; i = i++);
 // console.log( this)
+// TIME AND COLORS INDICATING TIME 
+console.log(currentHour)
+
+for (var i = 9; i < 18; i++){
+    var timeBlock = $('#time'+i)
+    if (i < currentHour){
+        timeBlock.addClass ("past")
+    }
+}
+for (var i = 9; i < 18; i++){
+    var timeBlock = $('#time'+i)
+    if (i == currentHour){
+        timeBlock.addClass ("present")
+    }
+}
+for (var i = 9; i < 18; i++){
+    var timeBlock = $('#time'+i)
+    if (i > currentHour){
+        timeBlock.addClass ("future")
+    }
+}
 
 
 
 
+// 🔒 locked
 
+// 🔓 unlocked
+
+// ✅ scheduled 
 // hover over to show locked or unlocked image
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work Day Scheduler Starter Code
+
+https://rakky21.github.io/apps/
+
+https://github.com/rakky21/apps
